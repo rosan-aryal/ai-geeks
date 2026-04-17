d0be929bd2c7de87639bc17ae9251a52e8d025da
Escobar <roshanjr464@gmail.com>
2026-04-17 21:21:59 +0545

    chore(test): remove unused jest setup file and invalid option

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: "jest-expo",
-  setupFilesAfterEach: ["<rootDir>/jest-setup.ts"],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?/.*|@react-navigation/.*|@tanstack/.*|nativewind|react-native-css)/)",
   ],
